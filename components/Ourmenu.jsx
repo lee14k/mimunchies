1e9b16d04669c2bb5d266da8d46c315d16085da6
lee14k <kmkovach@mtu.edu>
2023-07-22 14:38:07 -0400

    trying to get pix full width

--- components/Ourmenu.jsx
+++ components/Ourmenu.jsx
@@ -1,5 +1,18 @@
 export default function Ourmenu () {
     return (
-        <h1>Our Menu</h1>
+      <div>
+<h1>Our Menu</h1>
+
+<ul>
+    <li>
+        Candies & Treats
+    </li>
+    <li>
+        Fruits & Veggies
+    </li>
+   
+</ul>
+
+      </div>
     )
 }
